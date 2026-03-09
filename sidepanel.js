c85e9c77aaf22b10d32d1c5ca7ca8adf1b8d3871
mjulstein <6736558+mjulstein@users.noreply.github.com>
2026-03-09 19:29:00 +0100

    side-panel working

---
+++ sidepanel.js
@@ -0,0 +1,6 @@
+const testBtn = document.getElementById("testBtn");
+const output = document.getElementById("output");
+
+testBtn.addEventListener("click", () => {
+    output.textContent = "Side panel is working.";
+});
